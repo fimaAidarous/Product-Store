0520cd12468aa8ea13317bbf48058f5870f9be3a
fimaAidarous <fma0899@gmail.com>
2025-02-11 22:37:57 -0800

    MongoDB Setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+dotenv.config(); 
 
 const app = express();
 
+app.get("/products", (req,res) => {});
+
+// console.log(process.env.MONGO_URL);
+
 app.listen(800, () => {
+    connectDB();
     console.log("server started at http://localhost:800");
 });
-
